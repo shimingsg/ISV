5158daf9e2d9bbeb09d597f6b009c8bdffeb2b96
Shiming Ge <geshiming@outlook.com>
2019-02-15 15:08:41 +0800

    xxxxxxx

--- src/ISV/Pages/Issues/Index.cshtml.cs
+++ src/ISV/Pages/Issues/Index.cshtml.cs
@@ -13,13 +13,14 @@ using System.Threading.Tasks;
 
 namespace ISV.Pages.Issues
 {
-    public class IndexModel : IVPageModel
+    public class IndexModel : IVPaginationModel
     {
         private readonly IGithubService _githubservice;
         public IndexModel(AppDbContext context, IGithubService githubservice, ILoggerFactory loggerFactory, IConfiguration config)
             : base(context, loggerFactory, config)
         {
             _githubservice = githubservice;
+            Title = "Issues - List";
         }
 
         public IList<Issue> Issue { get; set; }
@@ -32,16 +33,23 @@ namespace ISV.Pages.Issues
         [BindProperty(SupportsGet = true)]
         public string SearchIssueId { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int? currentPage)
         {
             var issues = from m in _context.Issues
                          select m;
             if (!string.IsNullOrEmpty(SelectedCategoryId))
             {
                 var categoryId = Convert.ToInt32(SelectedCategoryId);
+
                 if (categoryId >= 0)
                 {
-                    issues = issues.Where(s => s.CategoryId == categoryId);
+                    var children = await GetCategoryChildren(categoryId);
+                    if (children.Count == 0)
+                        issues = issues.Where(s => s.CategoryId == categoryId);
+                    else
+                    {
+                        issues = issues.Where(x => children.Any(s => s.Id == x.CategoryId) || x.CategoryId == categoryId);
+                    }
                 }
             }
 
@@ -54,8 +62,11 @@ namespace ISV.Pages.Issues
                 }
             }
 
-            issues = issues.OrderByDescending(s => s.LastUpdatedAt);
-
+            CurrentPage = currentPage ?? 1;
+            await UpdatePageVariablesAsync<Issue>();
+            issues = issues.OrderByDescending(s => s.LastUpdatedAt)
+                            .Skip((CurrentPage - 1) * PageSize)
+                            .Take(PageSize);
             Issue = await issues.Include(i => i.Category).ToListAsync();
 
             SelectingCategories =
@@ -65,6 +76,11 @@ namespace ISV.Pages.Issues
                                                     Value = a.Id.ToString(),
                                                     Text = a.Name
                                                 }).ToListAsync();
+        }
+
+        private async Task<List<Category>> GetCategoryChildren(int id)
+        {
+            return await _context.Categories.Where(t => t.ParentId == id).ToListAsync();
         }
     }
 }
